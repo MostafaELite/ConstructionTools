4b2f18fd29ddcc044f0260d5fbf9c021a0a22186
Mostafa Mohamed Ahmed <mmahmed@thiqah.sa>
2019-05-05 23:01:07 +0300

    -Cleaning IndexController.js

--- CnstructionTools.Web/Scripts/IndexController.js
+++ CnstructionTools.Web/Scripts/IndexController.js
@@ -1,5 +1,5 @@
 ﻿app.controller('indexController',
-    function ($scope, $http, $compile) {
+    function ($scope, $http) {
         $scope.loading = true;
         $scope.indexResources = indexResources;
 
@@ -10,48 +10,67 @@
                 console.log(response);
             });
 
-        let rentingDays;
+        let rentingDays,selectedToolId;
         $scope.rent = (toolId) => {
+            selectedToolId = toolId;
             Swal.queue([
                 {
                     title: indexResources.popupTitle,
                     confirmButtonText: 'Calculate Fees',
                     input: 'number',
                     showLoaderOnConfirm: true,
-                    preConfirm: (numberOfRentingDays) => {
-                        rentingDays = numberOfRentingDays;
-                        debugger;
-                        return fetch(urls.calculateFees + `${toolId}/${numberOfRentingDays}`)
-                            .then(response => response.json())
-                            .then(data =>
-                                Swal.insertQueueStep({
-                                    title: indexResources.feesInfoMessage(data),
-                                    confirmButtonText: 'Rent',
-                                    showCancelButton: true
-                                })
-                            )
-                            .catch((e) => {
-                                console.log(e);
-                                Swal.insertQueueStep({
-                                    type: 'error',
-                                    title: 'Unable to calculate fees'
-                                });
-                            });
-                    }
+                    preConfirm: (numberOfRentingDays) => getRentCost(numberOfRentingDays, toolId)
                 }
-            ]).then(result => {
-                //User clicked ok and gone to rent and clicked rent button
-                if (result.value.length === 2 && result.value[1] == true)
-                    $http.post(urls.addNewSoppingCartItem + `${toolId}/${rentingDays}`)
-                        .then((response) => {
-                            response.data == true
-                                ? Swal.fire('Done!', indexResources.itemAddedSuccessfuly, 'success')
-                                : Swal.fire('Error!', 'Error adding your item , please try again later', 'error');
+            ])
+                .then(sendRentRequest);
 
-                        });
+        }
 
+
+        //----------------------------------Handlers--------------------------------------------------
+
+        //Gets the rent cost for a specific tool for a period from the server the number of renting days
+        function getRentCost(numberOfRentingDays) {
+            rentingDays = numberOfRentingDays;
+            debugger;
+            return fetch(urls.calculateFees + `${selectedToolId}/${numberOfRentingDays}`)
+                .then(response => response.json())
+                .then(showRentCost)
+                .catch(handleGetRentInfoFailure);
+        }
+
+        //Show a Swal modal containing the rent cost retrived from the server 
+        function showRentCost(data) {
+            Swal.insertQueueStep({
+                title: indexResources.feesInfoMessage(data),
+                confirmButtonText: 'Rent',
+                showCancelButton: true
+            });
+
+        }
+
+        //Show a Swal modal in case of error
+        function handleGetRentInfoFailure(error) {
+            console.log(error);
+            Swal.insertQueueStep({
+                type: 'error',
+                title: 'Unable to calculate fees'
             });
         }
+
+        //Handles the ok click event for the Swal rent cost modal
+        function sendRentRequest(result) {
+            //User clicked ok and gone to rent and clicked rent button
+            if (result.value.length === 2 && result.value[1] == true)
+                $http.post(urls.addNewSoppingCartItem + `${selectedToolId}/${rentingDays}`)
+                    .then((response) => {
+                        response.data == true
+                            ? Swal.fire('Done!', indexResources.itemAddedSuccessfuly, 'success')
+                            : Swal.fire('Error!', 'Error adding your item , please try again later', 'error');
+
+                    });
+
+        };
 
 
     });
